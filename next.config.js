db81386ac0b046cfc8b90f055e1be37f51b9fc2e
StarNumber12046 <64470722+StarNumber12046@users.noreply.github.com>
2025-02-08 20:18:35 +0100

    Ignore type errors in build

--- next.config.js
+++ next.config.js
@@ -28,6 +28,12 @@ const config = {
   },
   // This is required to support PostHog trailing slash API requests
   skipTrailingSlashRedirect: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default config;
